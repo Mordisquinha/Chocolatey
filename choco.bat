f3580556c242dd7f21d84ce0f7d2858a4a76e5b7
Santiago-San <dev.gsantiago@gmail.com>
2022-10-04 17:58:01 -0300

    ultimate version, only paste the repos

--- choco.bat
+++ choco.bat
@@ -1,25 +1,19 @@
-if (choco){
-    "choco alredy installed"
-} else {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-}
+:: Install choco .exe and add choco to PATH
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-$Folder = '~\Documents\Mordisquinha'
+::install git and clone all my repos in my github
+choco install git -fv
+cd "%userprofile% \Documents\Mordisquinha"
+git clone https://github.com/Mordisquinha/Chocolatey.git
+git clone https://github.com/Mordisquinha/Mordisquinha.git
+git clone https://github.com/Mordisquinha/Projeto_Bix.git
+git clone https://github.com/Mordisquinha/curriculo.git
+git clone https://github.com/Mordisquinha/Projeto_Pulsus.git
+git clone https://github.com/Mordisquinha/Jogo_da_Velha.git
 
-if (Test-Path -Path $Folder) {
-    "Path alredy exists!"
-} else {
-    mkdir Mordisquinha
-}
-
-cd $Folder
-
-if (git){
-    git clone https://github.com/Mordisquinha/*.git
-} else {
-    choco install git -Y -V
-}
-
-
+:: install all my anothers packages and reboot the computer
 choco install discord googlechrome vscode winrar wsl-ubuntu-2004 virtualbox notepadplusplus python dbeaver putty rainmeter lively vim winscp -Y -V 
-shutdown /r /t 0
+echo rebooting in 5s...
+
+shutdown /r /t 5
+
