b9a8161d2087ef01a9cfd9cd9a2b72a4ca3fba1f
Santiago-San <dev.gsantiago@gmail.com>
2022-10-04 12:32:43 -0300

    all logic to start

--- choco.bat
+++ choco.bat
@@ -1,2 +1,25 @@
-choco install discord git googlechrome vscode winrar wsl-ubuntu-2004 virtualbox notepadplusplus python dbeaver putty rainmeter lively vim winscp -Y -V 
+if (choco){
+    "choco alredy installed"
+} else {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+$Folder = '~\Documents\Mordisquinha'
+
+if (Test-Path -Path $Folder) {
+    "Path alredy exists!"
+} else {
+    mkdir Mordisquinha
+}
+
+cd $Folder
+
+if (git){
+    git clone https://github.com/Mordisquinha/*.git
+} else {
+    choco install git -Y -V
+}
+
+
+choco install discord googlechrome vscode winrar wsl-ubuntu-2004 virtualbox notepadplusplus python dbeaver putty rainmeter lively vim winscp -Y -V 
 shutdown /r
