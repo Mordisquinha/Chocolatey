f3d72cf56ab0e2995eb6fafcc3b8544327023860
Santiago-San <dev.gsantiago@gmail.com>
2022-10-04 10:11:41 -0300

    shutdown

--- choco.bat
+++ choco.bat
@@ -1,1 +1,2 @@
-choco install discord git googlechrome vscode winrar wsl-ubuntu-2004 virtualbox notepadplusplus python dbeaver putty rainmeter lively vim winscp -Y -V
+choco install discord git googlechrome vscode winrar wsl-ubuntu-2004 virtualbox notepadplusplus python dbeaver putty rainmeter lively vim winscp -Y -V 
+shutdown /r
